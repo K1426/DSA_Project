2402be1ce416d143aef0fac2cbc27f022a63e7aa
K1426 <ksohail.bese24seecs@seecs.edu.pk>
2025-11-25 14:11:06 +0500

    fetch data from multiple files

--- fetch.cpp
+++ fetch.cpp
@@ -1,24 +1,80 @@
 #include "rapidjson/document.h"
 #include "rapidjson/filereadstream.h"
-#include <iostream>
- 
+#include <filesystem>
+#include <bits/stdc++.h>
 using namespace rapidjson;
+namespace fsys = std::filesystem;
+
+std::string listFiles(std::string path, int& n, int& length)
+{
+    const fsys::path target_path{path};
+    std::string list = "";
+    n = 0;
+    try
+    {
+        for (auto const& dir_entry : fsys::directory_iterator{target_path})
+        {
+            if (fsys::is_regular_file(dir_entry.path()))
+                list+= dir_entry.path().filename().string();
+            n++;
+        }
+        length = list.length() / n;
+    }
+    catch (fsys::filesystem_error const& ex)
+    {
+        std::cout << "Error occurred during file operations!\n" << ex.what() << std::endl;
+        list = "";
+    }
+    return list;
+}
+
+std::string fetchData(const char* fname)
+{
+    char content[65536];
+    std::string text = "";
+    try
+    {
+        FILE* file = fopen(fname, "rb");
+        
+        FileReadStream istream(file, content, sizeof(content));
+        Document document;
+        
+        document.ParseStream(istream);
+        fclose(file);
+
+        text += document["metadata"]["title"].GetString();
+        text += "\n\n";
+        for (const auto& paragraph : document["abstract"].GetArray())
+        {
+            text += paragraph["text"].GetString();
+            text += "\n\n";
+        }
+        
+        for (const auto& paragraph : document["body_text"].GetArray())
+        {
+            text += paragraph["text"].GetString();
+            text += "\n\n";
+        }
+    }
+    catch (std::exception e)
+    {
+        std::cout << "There was an error in the formatting of the file " << fname << "\n";
+        text = "";
+    }
+    return text;
+}
 
 int main()
 {
-    FILE* file = fopen("myfile.json", "rb");
-    char content[65536];
-    std::string text = "";
-    FileReadStream istream(file, content, sizeof(content));
-    Document document;
+    int n = 0, length = 0;
+    std::string path = "D:\\cord-19_2020-06-01\\2020-06-01\\sample\\", list = listFiles(path, n, length);
+    std::string *file_list = new std::string[n], *content_list = new std::string[n];
     
-    document.ParseStream(istream);
-    fclose(file);
-
-    std::cout << document["paper_id"].GetString() << std::endl << std::endl;
-    std::cout <<document["abstract"][0]["text"].GetString() << std::endl << std::endl;
+    for (int i = 0; i < n; i++)
+        file_list[i] = list.substr(i*length, length);
+    for (int i = 0; i < n; i++)
+        content_list[i] = fetchData((path + file_list[i]).c_str());
     
-    for (const auto& paragraph : document["body_text"].GetArray())
-        text += paragraph["text"].GetString() + "\n\n";
+    std::cout << content_list[0];
     return 0;
 }
