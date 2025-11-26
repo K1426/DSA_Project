eb4f2ae61997ef28e0abefa0e08f8603cb9e8b9a
K1426 <ksohail.bese24seecs@seecs.edu.pk>
2025-11-26 20:48:33 +0500

    make lexicon

--- fetch.cpp
+++ fetch.cpp
@@ -7,8 +7,8 @@ namespace fsys = std::filesystem;
 
 void clean(std::string& s)
 {
-    s.erase(std::remove_if(s.begin(), s.end(), [](char c) { return ispunct(c); }), s.end());
-    if (s.length() < 3 || std::all_of(s.begin(), s.end(), isdigit)) s = "a";
+    s.erase(std::remove_if(s.begin(), s.end(), [](char c) { return !(isalnum(c) || c == '-' || c == '@'); }), s.end());
+    if (s.length() < 3 || std::all_of(s.begin(), s.end(), {return !isalpha(c);})) s = "a";
 }
 
 std::string listFiles(std::string path, int& n, int& length)
@@ -74,7 +74,7 @@ void addLexicon(std::string content)
 {
     std::stringstream ss;
     std::ofstream file;
-    std::string path = "D:\\cord-19_2020-06-01\\2020-06-01\\DSA_Project\\lexicon\\", word = "";
+    std::string path = "D:\\DSA_Project\\DSA_Project\\lexicon\\", word = "";
     int i = 0;
     
     fsys::create_directory(path);
@@ -97,15 +97,16 @@ void addLexicon(std::string content)
 int main()
 {
     int n = 0, length = 0, i = 0;
-    std::string path = "D:\\cord-19_2020-06-01\\2020-06-01\\DSA_Project\\sample\\", list = listFiles(path, n, length), content = "";
+    std::string path = "D:\\DSA_Project\\DSA_Project\\sample\\", list = listFiles(path, n, length), content = "";
     std::string *file_list = new std::string[n];
     
     
     for (i = 0; i < n; i++)
         file_list[i] = list.substr(i*length, length);
-    //for (i = 0; i < n; i++)
-        content = fetchData((path + file_list[0]).c_str());
-    
+    for (i = 0; i < n; i++)
+    {
+        content = fetchData((path + file_list[i]).c_str());
         addLexicon(content);
+    }
     return 0;
 }
