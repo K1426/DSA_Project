73ed11a85a9535df01b55ca07c452bf5001ddf41
mahnoorT49 <mtarar.bese24seecs@seecs.edu.pk>
2025-12-12 10:34:12 +0500

    Adding word search

--- implementation.cpp
+++ implementation.cpp
@@ -1,9 +1,36 @@
 #include <bits/stdc++.h>
 #include <filesystem>
 #include "Lexicon_Forward.h"
+#include "inverted_index.h"
+#include "algo.h"
 
 namespace fsys = std::filesystem;
 using namespace rapidjson;
+
+void rank(std::vector<std::vector<int>>& hitlist)
+{
+    int n = hitlist.size(), j = 0, i = 0;
+    quickSort(hitlist, 0, n-1);
+    j = 0;
+    while (j < n)
+    {
+        while (hitlist[i].size() == hitlist[j].size()) i++;
+        quickSort(hitlist, j, i-1, 0);
+        j = i;
+    }
+    for (i = 0; i < n; i++) std::cout << hitlist[i][0]<< "\n";
+}
+
+int single_search(std::string& word)
+{
+    clean_token(word);
+    if (word == "") return -1;
+    int wordID = lexicon[word];
+    std::cout << "word id " << wordID << std::endl;
+    std::vector<std::vector<int>> hitlist = barrel_inverted_index->get_word(wordID)->hitlist;
+    rank(hitlist);
+    return hitlist.size();
+}
 
 int main()
 {
@@ -22,13 +49,20 @@ int main()
     {
         std::cout << "\nCompleted Successfully!\n";
         std::cout << "Files processed: " << processed << "\n";
-        std::cout << "Lexicon size: " << lexicon.size() << "\n";
+        std::cout << "Lexicon size: " << current_wordID - 1 << "\n";
         std::cout << "Next available ID: " << current_wordID + 1 << "\n";
         std::cout << "Output saved in:\n";
         std::cout << "  - " << lexicon_file << "\n";
         std::cout << "  - " << forward_index_file << "\n";
     }
 
+    build_inverted_index();
+    save_inverted_index();
+    load_barrels();
+
+    std::string query = "something";
+    int n = single_search(query);
+    std::cout << "size " << n << std::endl;
     return 0;
 }
 
