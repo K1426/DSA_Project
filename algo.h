f8929941eb8cdcfd5b2408187d3b6bab3d6d4308
K1426 <ksohail.bese24seecs@seecs.edu.pk>
2025-12-14 17:31:37 +0500

    pass vectors by reference to sorting algo

--- algo.h
+++ algo.h
@@ -35,21 +35,21 @@ void swap(Doc& a, Doc& b)
     b = temp;
 }
 
-void quickSort(std::vector<std::vector<int>> a, int start, int end)
+void quickSort(std::vector<std::vector<int>>& a, int start, int end, int (*f)(std::vector<int>*))
 {
     //ensure the numbers to be sorted are more than one
     if (start >= end) return;
     
     //select pivot element (here it is center element)
-    int pivot = a[(start + end) / 2].size();
+    int pivot = f(&a[(start + end) / 2]);
     int i = start - 1, j = end + 1;
     
     while (true)
     {
         //decrease i until larger or equal element is found
-        do i++; while (a[i].size() < pivot);
+        do i++; while (f(&a[i]) < pivot);
         //decrease j until smaller or equal element is found
-        do j--; while (a[j].size() > pivot);
+        do j--; while (f(&a[j]) > pivot);
         //if all elements have been traversed break
         if (i >= j) break;
         //swap the elements that were on wrong side
@@ -57,34 +57,7 @@ void quickSort(std::vector<std::vector<int>> a, int start, int end)
     }
 
     //do the same with array on left of pivot
-    quickSort(a, start, j);
+    quickSort(a, start, j, f);
     //and then with array on right of pivot
-    quickSort(a, j + 1, end);
-}
-
-void quickSort(std::vector<std::vector<int>> a, int start, int end, int mode)
-{
-    //ensure the numbers to be sorted are more than one
-    if (start >= end) return;
-    
-    //select pivot element (here it is center element)
-    int pivot = a[(start + end) / 2][1];
-    int i = start - 1, j = end + 1;
-    
-    while (true)
-    {
-        //decrease i until larger or equal element is found
-        do i++; while (a[i][1] < pivot);
-        //decrease j until smaller or equal element is found
-        do j--; while (a[j][1]> pivot);
-        //if all elements have been traversed break
-        if (i >= j) break;
-        //swap the elements that were on wrong side
-        a[i].swap(a[j]);
-    }
-
-    //do the same with array on left of pivot
-    quickSort(a, start, j);
-    //and then with array on right of pivot
-    quickSort(a, j + 1, end);
+    quickSort(a, j + 1, end, f);
 }
